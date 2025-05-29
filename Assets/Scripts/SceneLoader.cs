c6a180d4bca9f1d399ec201378ecb1f5d6c8272e
KimKyungJun <1124kyungjun@naver.com>
2025-05-29 11:59:31 +0900

    인트로씬 UI

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,4 +7,12 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("IntroCinematic");
     }
+    public void OnClickSettings()
+    {
+        Debug.Log("환경설정 UI 열기"); 
+    }
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
 }
