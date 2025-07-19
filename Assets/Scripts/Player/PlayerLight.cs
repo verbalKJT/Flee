98bb77f204d3cb73f06f34d51dd73d8a26d0b1bc
dbdydgk <myday123456@gmail.com>
2025-07-19 13:50:48 +0900

    플레이어 랜턴 기능 추가
    F키를 눌러 랜턴을 키고 끌 수 있도록 추가했습니다.

---
+++ Assets/Scripts/Player/PlayerLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerLight : MonoBehaviour
+{
+    public Light lanternLight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (lanternLight != null)
+        {
+            lanternLight.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if(lanternLight != null)
+            {
+                lanternLight.enabled = !lanternLight.enabled;
+            }
+        }
+    }
+}
