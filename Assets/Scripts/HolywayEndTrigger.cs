e5ca6745c294ea77682b0fa3d40189ee062f1998
SHY9471 <lsgelt2050@naver.com>
2025-10-15 00:28:07 +0900

    holyway upgrade

--- Assets/Scripts/HolywayEndTrigger.cs
+++ Assets/Scripts/HolywayEndTrigger.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class HolywayEndTrigger : MonoBehaviour
@@ -9,7 +9,7 @@ public class HolywayEndTrigger : MonoBehaviour
     {
         owner = GetComponentInParent<holyway>();
         var col = GetComponent<Collider>();
-        col.isTrigger = true; // �ݵ�� Trigger
+        col.isTrigger = true; // 반드시 Trigger 이거 안하면 물체 그냥 지나가질수도있음
     }
 
     void OnTriggerEnter(Collider other)
