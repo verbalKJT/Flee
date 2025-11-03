ea5b6a4471ee43dcb1ecb2c6b9a59c03c7cd5aea
mutd441 <bjkim0752@gmail.com>
2025-11-04 00:29:28 +0900

    마네킹 바라보는 거리 구현

--- Assets/Scripts/WorkShop/Mannequin.cs
+++ Assets/Scripts/WorkShop/Mannequin.cs
@@ -5,19 +5,29 @@ public class Mannequin : MonoBehaviour
     [Header("플레이어 오브젝트")]
     public Transform player;
 
+    [Header("응시 거리")]
+    public float lookRange=3f;
     void Update()
     {
         if (player == null) return;
 
-        // 플레이어와의 방향 계산 (수평 회전만)
-        Vector3 direction = player.position - transform.position;
-        direction.y = 0f;
+        // 플레이어와의 거리 계산
+        float distance = Vector3.Distance(transform.position, player.position);
 
-        if (direction != Vector3.zero)
-        {            
-            Quaternion target = Quaternion.LookRotation(direction) * Quaternion.Euler(0f, 0f, 0f);
-            transform.rotation = target;
+        if (distance <= lookRange) 
+        {
+            // 플레이어와의 방향 계산 (수평 회전만)
+            Vector3 direction = player.position - transform.position;
+            direction.y = 0f;
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion target = Quaternion.LookRotation(direction) * Quaternion.Euler(0f, 0f, 0f);
+                transform.rotation = target;
+            }
         }
+
+        
     
     }
 
