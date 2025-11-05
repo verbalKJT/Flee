9a45a81063561d001caa744c980d3cdf9932f0fe
dbdydgk <myday123456@gmail.com>
2025-11-05 13:36:32 +0900

    손전등 키고 끌때 효과음 추가
    손전등 키고 끌때 효과음 추가

--- Assets/Scripts/Player/PlayerLight.cs
+++ Assets/Scripts/Player/PlayerLight.cs
@@ -5,6 +5,10 @@ public class FlashlightToggle : MonoBehaviour
     private Light flashlightLight;
     private bool isOn = false;
 
+    public AudioSource lightAudioSource;
+    public AudioClip lightOnClip;
+    public AudioClip lightOffClip;
+
     void Awake()
     {
         // 자식까지 전부 뒤져서 Light 하나 찾아옴
@@ -21,7 +25,13 @@ public class FlashlightToggle : MonoBehaviour
             isOn = !isOn;
 
             if (flashlightLight != null)
+            {
                 flashlightLight.enabled = isOn;
+                if (lightAudioSource != null && lightOnClip != null &&lightOffClip != null)
+                {
+                    lightAudioSource.PlayOneShot(isOn ? lightOnClip : lightOffClip);
+                }
+            }
         }
     }
 }
