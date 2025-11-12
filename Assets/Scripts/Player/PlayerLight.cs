1fb0c2f441fb5c9197b289ff669c45057257d713
dbdydgk <myday123456@gmail.com>
2025-11-13 03:08:20 +0900

    양초, 손전등 아이템 추가
    양초, 손전등 아이템 추가
    편지를 읽으면 책상위 양초 아이템을 얻는 것처럼 구현.
    미스테리 룸 화장대 위의 손전등 아이템을 E키를 눌러 얻도록 구현.

--- Assets/Scripts/Player/PlayerLight.cs
+++ Assets/Scripts/Player/PlayerLight.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public class FlashlightToggle : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class FlashlightToggle : MonoBehaviour
     public AudioClip lightOnClip;
     public AudioClip lightOffClip;
 
+    private PlayerLightInventory inv;
     void Awake()
     {
+        inv = GetComponent<PlayerLightInventory>();
         // 자식까지 전부 뒤져서 Light 하나 찾아옴
         flashlightLight = GetComponentInChildren<Light>(true);
 
@@ -20,6 +23,8 @@ public class FlashlightToggle : MonoBehaviour
 
     void Update()
     {
+        if (!inv.hasFlashlight) return;
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             isOn = !isOn;
