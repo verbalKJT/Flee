c8df3f29bd155297a356a1b39d80f2ba66f99f2a
verbalKJT <verbal0719@naver.com>
2025-11-18 20:20:16 +0900

    헤드 트레킹 막는 로직 변경

--- Assets/Scripts/VR/HeadTrackingLock.cs
+++ Assets/Scripts/VR/HeadTrackingLock.cs
@@ -5,18 +5,27 @@ public class HeadTrackingLock : MonoBehaviour
 {
     private OVRManager ovrManager;
     private bool isLockedTracking;
+
+    void Start()
+    {
+        ovrManager = FindObjectOfType<OVRManager>();
+    }
     private void SetHeadTrackingActive(bool IsActive)
     {
-        ovrManager = OVRManager.instance;
         if (ovrManager != null)
         {
             // 위치,회전 트래킹 비,활성화 
             ovrManager.usePositionTracking = IsActive;
             ovrManager.useRotationTracking = IsActive;
+            
+            // 트래킹 상태 설정
+            isLockedTracking = !IsActive;
+
             if (IsActive)
             {
                 // 트래킹 활성화 시 시점 중앙 재설정
                 OVRManager.display.RecenterPose();
+                Debug.Log("Recentered");
             }
         }
     }
@@ -40,11 +49,6 @@ public class HeadTrackingLock : MonoBehaviour
         {
             ovrManager.usePositionTracking = false;
             ovrManager.useRotationTracking = false;
-        }
-        else
-        {
-            ovrManager.usePositionTracking = true;
-            ovrManager.useRotationTracking = true;
         }
     }
 }
