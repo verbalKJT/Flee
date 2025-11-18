97773d47d8d0b4ea33e753722024c171a3a8f4df
김진태 <verbal0719@naver.com>
2025-11-18 18:44:14 +0900

    Revert "Feature/pcvr"

--- Assets/Scripts/Manager/MapInitializer.cs
+++ Assets/Scripts/Manager/MapInitializer.cs
@@ -52,12 +52,10 @@
             
             GameObject player = GameObject.FindGameObjectWithTag("Player"); // 플레이어 오브젝트
             
-            LetterCanvas = GameObject.Find("LetterCanvas"); 
-           
-			//PC 버전
-            //mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-            // VR 버전 Cam
-            mainCamera = GameObject.FindGameObjectWithTag("VRCam");
+            LetterCanvas = GameObject.Find("LetterCanvas");
+            
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            
             monsterPanel = GameObject.Find("InteractionCanvas").transform.Find("MiddleMonPanel").gameObject;       
 
         
