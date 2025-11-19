e3b1790331cb53d4162659d9734eb8ea82b4d98e
KimKyungJun <1124kyungjun@naver.com>
2025-11-19 18:08:39 +0900

    🏃‍VR 달리기 버튼 구현

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -49,7 +49,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 input = new Vector3(h, 0, v);
         Vector3 move = transform.TransformDirection(input.normalized);
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);       //달리기 키 입력(LShift)
+        //달리기 키 입력(LShift)
+        bool isRunning = Input.GetKey(KeyCode.LeftShift)|| 
+                         OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch);;       
 
         //스태미나가 0이하면 달리지 못하도록 설정
         if (!stamina.CanRun)
