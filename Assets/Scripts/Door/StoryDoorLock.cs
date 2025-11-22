a29baa0a95438c55e9373d0e902d67f0cbeb1ff7
mutd441 <bjkim0752@gmail.com>
2025-11-22 21:59:58 +0900

    스토리용 문 조건 스크립트 생성

---
+++ Assets/Scripts/Door/StoryDoorLock.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StoryDoorLock : MonoBehaviour
+{
+    [Header("문을 열기위한 스토리아이템 개수")]
+    public int ItemCount;
+    /*인벤토리 스크립트를 가져와서 갯수 체크를 해야함
+     * 아이템 등록될때마다 카운트하기
+     * 죽었을때는 담주에 고민+인벤토리 초기화함수
+     */
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
