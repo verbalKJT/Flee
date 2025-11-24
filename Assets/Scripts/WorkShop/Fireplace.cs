41138b4cea160885d9daddb37fa0c74f665aaeaa
mutd441 <bjkim0752@gmail.com>
2025-11-25 01:18:42 +0900

    마지막복도문 조건 구현

--- Assets/Scripts/WorkShop/Fireplace.cs
+++ Assets/Scripts/WorkShop/Fireplace.cs
@@ -8,7 +8,10 @@ public class Fireplace : MonoBehaviour
         {
             
             Debug.Log("몬스터 벽난로 충돌");
+            //몬스터 비활성화
             other.gameObject.SetActive(false);
+            //기믹 수행 체크
+            Inventory.Instance.MonInFireplace=true;
             
         }
        
