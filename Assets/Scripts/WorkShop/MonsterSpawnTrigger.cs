80b15ac6065be88aec091d4c469ebac5a840dd3e
mutd441 <bjkim0752@gmail.com>
2025-11-24 22:47:56 +0900

    스토리아이템 수집 조건검사 구현

--- Assets/Scripts/WorkShop/MonsterSpawnTrigger.cs
+++ Assets/Scripts/WorkShop/MonsterSpawnTrigger.cs
@@ -6,7 +6,17 @@ public class MonsterSpawnTrigger : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        Debug.Log("작업실 몬스터 스폰!");
+        
+        //스토리아이템 수집 완료 검사
+        if (Inventory.Instance.hasAllStoryItems)
+        {
+            Debug.Log("아이템 6개 수집 완료"+ Inventory.Instance.hasAllStoryItems);
+            Debug.Log("작업실 몬스터 스폰!");
+        }
+        else
+        {
+            Debug.Log("아이템 수집 개수가 부족합니다!"+Inventory.Instance.CheckAllStoryItemsCollected());
+        }
     }
 
     public string GetPromptText()
