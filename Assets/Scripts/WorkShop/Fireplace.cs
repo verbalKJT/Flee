9aafad6f28776dc9b438885f5a0e7acdaaff43f0
mutd441 <bjkim0752@gmail.com>
2025-12-01 00:43:34 +0900

    Update Fireplace.cs

--- Assets/Scripts/WorkShop/Fireplace.cs
+++ Assets/Scripts/WorkShop/Fireplace.cs
@@ -6,8 +6,10 @@ public class Fireplace : MonoBehaviour
     {
         if (other.CompareTag("WorkshopMon"))
         {
-            
+            WorkshopMon mon = other.GetComponent<WorkshopMon>();
             Debug.Log("몬스터 벽난로 충돌");
+            //몬스터 소리 재생
+            mon.EnterFireplace();
             //몬스터 비활성화
             other.gameObject.SetActive(false);
             //기믹 수행 체크
